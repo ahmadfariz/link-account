28e4dc211078c27ecfc3574adb5d88f6b8b3ea90
unknown <ahmudhakim@gmail.com>
2021-11-25 07:27:52 +0700

    link-accont

---
+++ Linktree 1/src/assets/items.js
@@ -0,0 +1,40 @@
+// import website from '../assets/website.png'
+import instagram from '../assets/instagram.png'
+import github from '../assets/github.png'
+import steam from '../assets/steam.png'
+import facebook from '../assets/facebook.png'
+
+const items = [
+// {
+//     "title": "Website",
+//     "subtitle": "🌐 Look at my work!",
+//     "image": website,
+//     "link": "https://sagniksahoo.me" //your personal website or portfolio  link
+// },
+{
+    "title": "GitHub",
+    "subtitle": "@ahmadfariz | 🏠 of my open-source projects",
+    "image": github,
+    "link": "https://github.com/ahmadfariz"
+},
+{
+    "title": "Facebook",
+    "subtitle": "Let's be friends 🤝",
+    "image": facebook,
+    "link": "https://www.facebook.com/ahmadfarizdwiputra"
+},
+{
+    "title": "Instagram",
+    "subtitle": "@ahmadfarizdwiputra | Don't forget to follow me 😉",
+    "image": instagram,
+    "link": "https://www.instagram.com/ahmadfarizdwiputra/"
+},
+{
+    "title": "Steam",
+    "subtitle": "🎮 Come play with me",
+    "image": steam,
+    "link": "https://steamcommunity.com/id/ahmadfariz/"
+}
+]
+
+export default items
